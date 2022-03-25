d4fdf3700c2ad712d289e7524d1e068132672b7d
AreyBRSK <areybrsk@gmail.com>
2022-03-25 12:22:12 +0300

    Initial setup

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var currentColorView: UIView!
+    
+    @IBOutlet var colorSliderRed: UISlider!
+    @IBOutlet var colorSliderGreen: UISlider!
+    @IBOutlet var colorSliderBlue: UISlider!
+    
+    @IBOutlet var colorLabelRed: UILabel!
+    @IBOutlet var colorLabelGreen: UILabel!
+    @IBOutlet var colorLabelBlue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resetToDefault()
+        
     }
 
+    @IBAction func colorSliderRedControl() {
+    }
+    @IBAction func colorSliderGreenControl() {
+    }
+    @IBAction func colorSliderBlueControl() {
+    }
+    
+    
+    private func resetToDefault() {
+        colorSliderRed.value = 0
+        colorSliderGreen.value = 0
+        colorSliderBlue.value = 0
+        
+    }
 
 }
 
