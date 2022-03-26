897839793384995955fc68062414f5180e8ceadd
AreyBRSK <areybrsk@gmail.com>
2022-03-26 09:25:33 +0300

    Code clean-up after feedback

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -7,12 +7,6 @@
 
 import UIKit
 
-var colorRedValue: Float = 0
-var colorGreenValue: Float = 0
-var colorBlueValue: Float = 0
-
-let defaultCornerRadius: CGFloat = 15
-
 class ViewController: UIViewController {
     
     @IBOutlet var currentColorView: UIView!
@@ -31,7 +25,6 @@ class ViewController: UIViewController {
     @IBOutlet var colorButtonReset: UIButton!
     @IBOutlet var colorButtonRandom: UIButton!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -41,17 +34,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func colorSliderRedControl() {
-        colorRedValue = colorSliderRed.value
         colorLabelRed.text = String(Int(colorSliderRed.value))
         updateCurrentColorView()
     }
     @IBAction func colorSliderGreenControl() {
-        colorGreenValue = colorSliderGreen.value
         colorLabelGreen.text = String(Int(colorSliderGreen.value))
         updateCurrentColorView()
     }
     @IBAction func colorSliderBlueControl() {
-        colorBlueValue = colorSliderBlue.value
         colorLabelBlue.text = String(Int(colorSliderBlue.value))
         updateCurrentColorView()
     }
@@ -70,66 +60,63 @@ class ViewController: UIViewController {
     
     
     private func resetColorToDefault() {
-        colorRedValue = 0
-        colorGreenValue = 0
-        colorBlueValue = 0
+        colorSliderRed.value = colorSliderRed.minimumValue
+        colorSliderGreen.value = colorSliderGreen.minimumValue
+        colorSliderBlue.value = colorSliderBlue.minimumValue
     }
     
     private func updateCurrentColorView() {
-        currentColorView.backgroundColor  = .init(red: CGFloat(colorRedValue / 255), green: CGFloat(colorGreenValue / 255), blue: CGFloat(colorBlueValue / 255), alpha: 1)
+        currentColorView.backgroundColor  = UIColor(
+            red: CGFloat(colorSliderRed.value / 255),
+            green: CGFloat(colorSliderGreen.value / 255),
+            blue: CGFloat(colorSliderBlue.value / 255),
+            alpha: 1)
     }
     
     private func updateUI() {
         
-        colorSliderRed.value = colorRedValue
-        colorSliderGreen.value = colorGreenValue
-        colorSliderBlue.value = colorBlueValue
-        
-        colorLabelRed.text = String(Int(colorRedValue))
-        colorLabelGreen.text = String(Int(colorGreenValue))
-        colorLabelBlue.text = String(Int(colorBlueValue))
+        colorLabelRed.text = String(Int(colorSliderRed.value))
+        colorLabelGreen.text = String(Int(colorSliderGreen.value))
+        colorLabelBlue.text = String(Int(colorSliderBlue.value))
     }
     
     private func generateRandomColor() {
-        colorRedValue = .random(in: 0...255)
-        colorGreenValue = .random(in: 0...255)
-        colorBlueValue = .random(in: 0...255)
+        colorSliderRed.value = .random(in: 0...255)
+        colorSliderGreen.value = .random(in: 0...255)
+        colorSliderBlue.value = .random(in: 0...255)
     }
     
     private func configUI() {
         
         // Current color view
         
-        currentColorView.layer.cornerRadius = defaultCornerRadius
+        currentColorView.layer.cornerRadius = 15
         currentColorView.layer.borderWidth = 1
         currentColorView.layer.borderColor = UIColor.lightGray.cgColor
         
         
         // Red slider
         
-        colorSliderRed.minimumValue = 0
         colorSliderRed.maximumValue = 255
         colorSliderRed.minimumTrackTintColor = .red.withAlphaComponent(0.5)
         colorSliderRed.thumbTintColor = .red
         
         // Green slider
         
-        colorSliderGreen.minimumValue = 0
         colorSliderGreen.maximumValue = 255
         colorSliderGreen.minimumTrackTintColor = .green.withAlphaComponent(0.5)
         colorSliderGreen.thumbTintColor = .green
         
         // Blue slider
         
-        colorSliderBlue.minimumValue = 0
         colorSliderBlue.maximumValue = 255
         colorSliderBlue.minimumTrackTintColor = .blue.withAlphaComponent(0.5)
         colorSliderBlue.thumbTintColor = .blue
         
         // Buttons
         
-        colorButtonReset.layer.cornerRadius = defaultCornerRadius
-        colorButtonRandom.layer.cornerRadius = defaultCornerRadius
+        colorButtonReset.layer.cornerRadius = 15
+        colorButtonRandom.layer.cornerRadius = 15
     }
 
 }
